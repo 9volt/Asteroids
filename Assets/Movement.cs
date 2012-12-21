17cb2c86781eac2a5ad6507ba32187be4bde7214
Joseph Utecht <thenmal@gmail.com>
2012-12-20 23:42:00 -0600

    Added textured ship

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,28 +2,24 @@ using UnityEngine;
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	private float speed;
-	private float maxSpeed = 5;
-	
 	// Use this for initialization
 	void Start () {
-		speed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Rotate the ship based on horizontal movement.
-		transform.Rotate(0, 0, -1 * Input.GetAxis("Horizontal"));
-		transform.Translate(0, Input.GetAxis("Vertical"), 0);
+		transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+		transform.Translate(0, 0, Input.GetAxis("Vertical"));
 		Vector3 position = transform.position;
 		if (position.x > 100){
-			transform.Translate(-100, position.y, 0);	
-		} else if (position.y > 100){
-			transform.Translate(position.x, -100, 0);	
+			transform.Translate(-100, 0, position.z);	
+		} else if (position.z > 100){
+			transform.Translate(position.x, 0, -100);	
 		} else if (position.x < -100){
-			transform.Translate(100, position.y, 0);	
-		} else if (position.y < -100){
-			transform.Translate(position.x, 100, 0);	
+			transform.Translate(100, 0, position.z);	
+		} else if (position.z < -100){
+			transform.Translate(position.x, 0, 100);	
 		}
 	}
 }
