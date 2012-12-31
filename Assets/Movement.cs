76552ed69c87d949469311d8d5c759c5abea87db
Quasar Jarosz <quasar@ja.rosz.org>
2012-12-31 16:08:58 -0600

    changed method of rotation

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -12,9 +12,10 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// Rotate the ship based on horizontal movement.
-		transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+		//transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
 		//transform.Translate(0, 0, Input.GetAxis("Vertical"));
 		rigidbody.AddForce(transform.forward * Input.GetAxis("Vertical") * 1000);
+		rigidbody.AddTorque(0, Input.GetAxis("Horizontal") * 1000, 0);
 		Vector3 position = transform.position;
 		if (Input.GetButtonDown("Fire1")) {
 			Instantiate(laserShot, position, transform.rotation);	
